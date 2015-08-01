3403144b60a54eb76925005b24a319a96376db3c
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2015-08-02 02:30:21 +0530

    initial commit nothing workis as of now

---
+++ index.js
@@ -0,0 +1,73 @@
+var http=require('http');
+var fs=require('fs');
+var url=require('url');
+var queryString=require('querystring');
+var API='api key';
+http.createServer(function(req,res)
+{
+	var raw = '';
+	var json={};
+	req.on('data', function(d) {
+	  raw += d; 
+	});
+	req.on('end', function() {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  if(raw)
+	  {
+	  	json=JSON.parse(raw);
+	  	console.log(parser(json,function(command,text)
+	  	{
+	  		sendMessage('received the text');
+	  	}));
+	  }
+	  res.end(raw);   
+	});
+	 
+}).listen(8181);
+
+var parser=function(json)
+{
+	var command='';
+	var text='';
+	if(!json.message)
+		return;
+	text=json.message.text;
+	text=text.replace(/^[ ]+/,'');
+	if(text.match(/^\//)){
+		//has command
+		command=text.split(' ')[0];
+		text=text.replace(command,'');
+	}
+
+	var params= {
+		command : command,
+		text	: text
+	};
+	if(!callback)
+		return params;
+	callback(params.command,params.text);
+};
+var tasker=function(command,text)
+{
+	
+};
+var sendMessage=function(msg)
+{
+	reactor('sendMessage',msg);
+};
+var reactor=function(method,queryString)
+{
+	if(!method)
+		return;
+	options={
+		host : 'https://api.telegram.org/bot'+API_KEY+'/'+method+'?'+queryString
+	};
+	var req=http.request(options,function(res)
+	{
+		
+	});
+	req.on('error',function(e)
+	{
+		console.log('problem with request '+e.message);
+	});
+};
